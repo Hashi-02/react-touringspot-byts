4f71c071fd58f800d9ef4de3170a3f5faca204d6
hashi-02 <hassie0209@gmail.com>
2022-10-06 09:54:15 +0900

    mapコンポーネントの作成反映

---
+++ src/components/SampleComponent.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import GoogleMapReact from 'google-map-react';
+/**
+ * Mapに使用するプロパティ
+ */
+interface MapProps {
+  center: {
+    lat: number;
+    lng: number;
+  };
+  zoom: number;
+}
+
+/**
+ * MapのPropsの初期値
+ */
+const initialMapProps: MapProps = {
+  center: {
+    lat: 35.39,
+    lng: 139.44,
+  },
+  zoom: 18,
+};
+
+/**
+ * APIキー
+ */
+const API_KEY: string = process.env.REACT_APP_GOOGLEMAP_API_KEY; // TODO: 自分のキーをここに入力
+
+/**
+ * サンプルとして地図を表示するコンポーネント
+ */
+const SampleMap = () => {
+  const [mapProps, setMapProps] = useState<MapProps>(initialMapProps);
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: API_KEY }}
+        center={mapProps.center}
+        zoom={mapProps.zoom}
+      />
+    </div>
+  );
+};
+
+export default SampleMap;

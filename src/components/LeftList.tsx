812a7c0ab39ac7708807468e8beb17cb9344ea68
hashi-02 <hassie0209@gmail.com>
2022-10-12 23:55:42 +0900

    ホームに戻るボタンを追加

--- src/components/LeftList.tsx
+++ src/components/LeftList.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Card } from './Card';
+import { Link } from 'react-router-dom';
 
 export const LeftList = () => {
   return (
     <>
       <div className="">
         <div className="text-center p-6">
+          <button className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+            <Link to="/">戻る</Link>
+          </button>
           <h1>検索結果をcardで表示するところ</h1>
         </div>
         <div className="p-3">

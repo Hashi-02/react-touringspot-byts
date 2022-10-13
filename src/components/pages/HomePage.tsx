dc8d475da2489516d7bc845fcc93d74186f3a04a
hashi-02 <hassie0209@gmail.com>
2022-10-14 01:56:28 +0900

    微修正

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 export const Home = () => {
   return (
     <>
-      <div>Home</div>
       <div>
         <button className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
           <Link to="maps">マップを見る</Link>
         </button>
+        <h1>Home</h1>
       </div>
     </>
   );
--- src/components/pages/MapPage.tsx
+++ src/components/pages/MapPage.tsx
@@ -4,10 +4,6 @@ import MapComponent from '../molecules/SampleComponent';
 export const MapPage = () => {
   return (
     <div className="flex flex-col h-screen">
-      {/* <div className="h-full"> */}
-      {/* <div className="sticky top-0 z-50 bg-white shadow-xl h-1/6">
-      <Navbar />
-    </div> */}
       <div className="flex h-full">
         <div className="flex-initial w-2/6 overflow-y-scroll">
           <LeftList />
@@ -17,6 +13,5 @@ export const MapPage = () => {
         </div>
       </div>
     </div>
-    // </div>)
   );
 };

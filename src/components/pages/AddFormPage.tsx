ed46dc3e5434232c75425b5dfe429f4c52dad449
hashi-02 <hassie0209@gmail.com>
2022-10-20 22:45:11 +0900

    FormPageの追加

---
+++ src/components/pages/AddFormPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BaseButton } from '../atoms/button/BaseButton';
+export const AddFormPage = () => {
+  return (
+    <>
+      <div>AddFormPage</div>
+      <BaseButton text="マップを見る" routing="/maps" />
+    </>
+  );
+};

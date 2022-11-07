8252a87a5b95a40a5c568b030539b797fba95753
hashi-02 <hassie0209@gmail.com>
2022-11-07 15:33:03 +0900

    descriptionのfieldを複数行のレイアウトに変更

--- src/components/pages/AddFormPage.tsx
+++ src/components/pages/AddFormPage.tsx
@@ -91,8 +91,10 @@ const InnerForm = (props: OtherProps & FormikProps<FormValues>) => {
         </div>
         <div className="md:w-2/3">
           <Field
+            as="textarea"
             type="text"
             name="description"
+            rows="6"
             className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
           />
         </div>

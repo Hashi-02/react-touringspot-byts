ee4f6566e4cacec89d9cf6952eba63f961765a01
hashi-02 <hassie0209@gmail.com>
2022-11-08 17:45:28 +0900

    エラーtouchedを追加

--- src/components/pages/AddFormPage.tsx
+++ src/components/pages/AddFormPage.tsx
@@ -1,201 +1,179 @@
-import { BaseButton } from '../atoms/button/BaseButton';
-import React from 'react';
-import { withFormik, FormikProps, FormikErrors, Form, Field } from 'formik';
-import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import * as React from 'react';
+import { Formik, Field, Form } from 'formik';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 import { db } from '../../firebase';
-
-// Shape of form values
-interface FormValues {
+import * as Yup from 'yup';
+import { BaseButton } from '../atoms/button/BaseButton';
+interface Values {
   placeName: string;
   file: string;
   description: string;
   Latitude: string;
   Longitude: string;
 }
-
-interface OtherProps {
-  message: string;
-}
-
-// Aside: You may see InjectedFormikProps<OtherProps, FormValues> instead of what comes below in older code.. InjectedFormikProps was artifact of when Formik only exported a HoC. It is also less flexible as it MUST wrap all props (it passes them through).
-const InnerForm = (props: OtherProps & FormikProps<FormValues>) => {
-  const { touched, errors, isSubmitting, message } = props;
-  return (
-    <Form className="w-full max-w-sm">
-      <h1>{message}</h1>
-      <div className="md:flex md:items-center mb-6">
-        <div className="md:w-1/3">
-          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-            地名
-          </label>
-        </div>
-        <div className="md:w-2/3">
-          <Field
-            type="text"
-            name="placeName"
-            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-          />
-          {touched.placeName && errors.placeName && (
-            <div>{errors.placeName}</div>
-          )}
-        </div>
-      </div>
-
-      <div className="md:flex md:items-center mb-6">
-        <div className="md:w-1/3">
-          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-            緯度
-          </label>
-        </div>
-        <div className="md:w-2/3">
-          <Field
-            type="text"
-            name="Latitude"
-            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-          />
-          {touched.Latitude && errors.Latitude && <div>{errors.Latitude}</div>}
-        </div>
-      </div>
-
-      <div className="md:flex md:items-center mb-6">
-        <div className="md:w-1/3">
-          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-            経度
-          </label>
-        </div>
-        <div className="md:w-2/3">
-          <Field
-            type="text"
-            name="Longitude"
-            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-          />
-          {touched.Longitude && errors.Longitude && (
-            <div>{errors.Longitude}</div>
-          )}
-        </div>
-      </div>
-
-      <div className="md:flex md:items-center mb-6">
-        <div className="md:w-1/3">
-          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-            ファイル
-          </label>
-        </div>
-        <div className="md:w-2/3">
-          <Field
-            type="text"
-            name="file"
-            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-          />
-          {touched.file && errors.file && <div>{errors.file}</div>}
-        </div>
-      </div>
-      <div className="md:flex md:items-center mb-6">
-        <div className="md:w-1/3">
-          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-            説明
-          </label>
-        </div>
-        <div className="md:w-2/3">
-          <Field
-            as="textarea"
-            type="text"
-            name="description"
-            rows="6"
-            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-          />
-          {touched.description && errors.description && (
-            <div>{errors.description}</div>
-          )}
-        </div>
-      </div>
-
-      <div className="md:flex md:items-center">
-        <div className="md:w-1/3"></div>
-        <div className="md:w-2/3">
-          <button
-            className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-            type="submit"
-            disabled={isSubmitting}
-          >
-            Submit
-          </button>
-        </div>
-      </div>
-    </Form>
-  );
+const handleSubmit = async (values: Values) => {
+  try {
+    const time = serverTimestamp();
+    console.log(values.placeName);
+    const usersCollectionRef = collection(db, 'users');
+    const documentRef = await addDoc(usersCollectionRef, {
+      placeName: values.placeName,
+      file: values.file,
+      description: values.description,
+      timestamp: time,
+      Longitude: values.Longitude,
+      Latitude: values.Latitude,
+    });
+    console.log(documentRef);
+  } catch (error) {
+    console.log('err', error);
+  }
+  console.log('err');
 };
 
-// The type of props MyForm receives
-interface MyFormProps {
-  message: string; // if this passed all the way through you might do this or make a union type
-}
+const validationSchema = Yup.object({
+  placeName: Yup.string()
+    .max(15, '１５文字以下で入力してください')
+    .required('このフィールドは必須です'),
+  description: Yup.string()
+    .max(15, '１５文字以下で入力してください')
+    .required('このフィールドは必須です'),
+  Longitude: Yup.string()
+    .max(15, '１５文字以下で入力してください')
+    .required('このフィールドは必須です'),
+  Latitude: Yup.string()
+    .max(50, '１５文字以下で入力してください')
+    .required('このフィールドは必須です'),
+});
 
-// Wrap our form with the withFormik HoC
-const MyForm = withFormik<MyFormProps, FormValues>({
-  // Transform outer props into form values
-  mapPropsToValues: (props) => {
-    return {
-      placeName: '',
-      file: '',
-      description: '',
-      Latitude: '',
-      Longitude: '',
-    };
-  },
+export const AddFormPage = () => {
+  const initialValues: Values = {
+    placeName: '',
+    file: '',
+    description: '',
+    Latitude: '',
+    Longitude: '',
+  };
 
-  // Add a custom validation function (this can be async too!)
-  validate: (values: FormValues) => {
-    let errors: FormikErrors<FormValues> = {};
-    if (!values.placeName) {
-      errors.placeName = 'このフィールドは必須です';
-    } else if (values.placeName.length > 15) {
-      errors.placeName = '１５文字以下で入力してください';
-    }
-    if (!values.description) {
-      errors.description = 'このフィールドは必須です';
-    } else if (values.description.length > 15) {
-      errors.description = '１５文字以下で入力してください';
-    }
-    if (!values.Latitude) {
-      errors.Latitude = 'このフィールドは必須です';
-    } else if (values.Latitude.length > 50) {
-      errors.Latitude = '１５文字以下で入力してください';
-    }
-    if (!values.Longitude) {
-      errors.Longitude = 'このフィールドは必須です';
-    } else if (values.Longitude.length > 50) {
-      errors.Longitude = '１５文字以下で入力してください';
-    }
-    return errors;
-  },
+  return (
+    <div>
+      <h1>AddFormPage</h1>
+      <BaseButton text="マップを見る" routing="/maps" />
+      <div className="flex justify-center">
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {(formik) => {
+            return (
+              <Form className="w-full max-w-sm" onSubmit={formik.handleSubmit}>
+                <div className="md:flex md:items-center mb-6">
+                  <div className="md:w-1/3">
+                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                      地名
+                    </label>
+                  </div>
+                  <div className="md:w-2/3">
+                    <Field
+                      type="text"
+                      name="placeName"
+                      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                      value={formik.values.placeName}
+                      onChange={formik.handleChange}
+                    />
+                    {formik.touched.placeName && formik.errors.placeName ? (
+                      <div>{formik.errors.placeName}</div>
+                    ) : null}
+                  </div>
+                </div>
 
-  handleSubmit: async (values) => {
-    try {
-      const time = serverTimestamp();
-      console.log(values.placeName);
-      const usersCollectionRef = collection(db, 'users');
-      const documentRef = await addDoc(usersCollectionRef, {
-        placeName: values.placeName,
-        file: values.file,
-        description: values.description,
-        timestamp: time,
-        Longitude: values.Longitude,
-        Latitude: values.Latitude,
-      });
-      console.log(documentRef);
-    } catch (error) {
-      console.log('err', error);
-    }
-  },
-})(InnerForm);
+                <div className="md:flex md:items-center mb-6">
+                  <div className="md:w-1/3">
+                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                      緯度
+                    </label>
+                  </div>
+                  <div className="md:w-2/3">
+                    <Field
+                      type="text"
+                      name="Latitude"
+                      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                    />
+                    {formik.touched.Latitude && formik.errors.Latitude ? (
+                      <div>{formik.errors.Latitude}</div>
+                    ) : null}
+                  </div>
+                </div>
 
-export const AddFormPage = () => (
-  <div>
-    <h1>AddFormPage</h1>
-    <BaseButton text="マップを見る" routing="/maps" />
-    <div className="flex justify-center">
-      <MyForm message="地名、名前、説明を記入" />
+                <div className="md:flex md:items-center mb-6">
+                  <div className="md:w-1/3">
+                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                      経度
+                    </label>
+                  </div>
+                  <div className="md:w-2/3">
+                    <Field
+                      type="text"
+                      name="Longitude"
+                      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                    />
+                    {formik.touched.Longitude && formik.errors.Longitude ? (
+                      <div>{formik.errors.Longitude}</div>
+                    ) : null}
+                  </div>
+                </div>
+
+                <div className="md:flex md:items-center mb-6">
+                  <div className="md:w-1/3">
+                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                      ファイル
+                    </label>
+                  </div>
+                  <div className="md:w-2/3">
+                    <Field
+                      type="text"
+                      name="file"
+                      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                    />
+                  </div>
+                </div>
+                <div className="md:flex md:items-center mb-6">
+                  <div className="md:w-1/3">
+                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                      説明
+                    </label>
+                  </div>
+                  <div className="md:w-2/3">
+                    <Field
+                      as="textarea"
+                      type="text"
+                      name="description"
+                      rows="6"
+                      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                    />
+                    {formik.touched.description && formik.errors.description ? (
+                      <div>{formik.errors.description}</div>
+                    ) : null}
+                  </div>
+                </div>
+
+                <div className="md:flex md:items-center">
+                  <div className="md:w-1/3"></div>
+                  <div className="md:w-2/3">
+                    <button
+                      className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                      type="submit"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </div>
+              </Form>
+            );
+          }}
+        </Formik>
+      </div>
     </div>
-  </div>
-);
+  );
+};

1a4eedff8e44a2b39ed568ab6f267c0451582513
hashi-02 <hassie0209@gmail.com>
2022-11-08 15:56:30 +0900

    testPageの作成 formの書き方を変更

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './components/pages/HomePage';
 import { DetailPage } from './components/pages/DetailPage';
 import { AddFormPage } from './components/pages/AddFormPage';
+import { TestPage } from './components/pages/TestPage';
 const App = () => (
   <BrowserRouter>
     <Routes>
@@ -11,6 +12,7 @@ const App = () => (
       <Route path="maps" element={<MapPage />} />
       <Route path="maps/detailid" element={<DetailPage />} />
       <Route path="add" element={<AddFormPage />} />
+      <Route path="test" element={<TestPage />} />
     </Routes>
   </BrowserRouter>
 );

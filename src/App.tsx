991ece7fe19789d33118cf6076ebd7f9a6dd9f3b
hashi-02 <hassie0209@gmail.com>
2022-11-11 23:40:24 +0900

    TestPage削除

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './components/pages/HomePage';
 import { DetailPage } from './components/pages/DetailPage';
 import { AddFormPage } from './components/pages/AddFormPage';
-import { TestPage } from './components/pages/TestPage';
 const App = () => (
   <BrowserRouter>
     <Routes>
@@ -12,7 +11,6 @@ const App = () => (
       <Route path="maps" element={<MapPage />} />
       <Route path="maps/detailid" element={<DetailPage />} />
       <Route path="add" element={<AddFormPage />} />
-      <Route path="test" element={<TestPage />} />
     </Routes>
   </BrowserRouter>
 );

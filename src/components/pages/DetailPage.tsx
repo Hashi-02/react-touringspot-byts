54bb7ba4449c95aad9fbe336a0965ef48fecca0e
hashi-02 <hassie0209@gmail.com>
2022-12-04 17:51:40 +0900

    詳細画面に対応した情報の表示

--- src/components/pages/DetailPage.tsx
+++ src/components/pages/DetailPage.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import { doc, getDoc } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { db } from '../../firebase';
 import { BaseButton } from '../atoms/button/BaseButton';
 import { ImageUploader } from '../molecules/ImageUploader';
 
 export const DetailPage = () => {
+  type TdetailInfo = {
+    placeName: string;
+    file: string;
+    description: string;
+    Latitude: string;
+    Longitude: string;
+  };
+  const [detailInfo, setDetailInfo] = useState<TdetailInfo>();
+  useEffect(() => {
+    const userDocumentRef = doc(db, 'users', '0Vdlc5IiaAsvXgK3mCZm');
+    getDoc(userDocumentRef).then((documentSnapshot) => {
+      if (documentSnapshot.exists()) {
+        const Info: TdetailInfo = {
+          placeName: documentSnapshot.data().placeName,
+          file: documentSnapshot.data().file,
+          description: documentSnapshot.data().description,
+          Latitude: documentSnapshot.data().Latitude,
+          Longitude: documentSnapshot.data().Longitude,
+        };
+        setDetailInfo(Info);
+      }
+    });
+  }, []);
   return (
     <>
       <BaseButton text="マップに戻る" routing="/maps" />
       <div className="container">
+        <p>{detailInfo?.placeName}</p>
         <ImageUploader />
       </div>
     </>

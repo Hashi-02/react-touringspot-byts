1e9dcd93792714cb8095f4568c6426be32021266
hashi-02 <hassie0209@gmail.com>
2022-12-11 23:03:35 +0900

    入力候補取得出力

--- src/components/pages/Test.tsx
+++ src/components/pages/Test.tsx
@@ -1,4 +1,5 @@
 import GoogleMapReact from 'google-map-react';
+import { useState } from 'react';
 interface MapProps {
   center: {
     lat: number;
@@ -12,40 +13,77 @@ const initialMapProps: MapProps = {
     lat: 35.39,
     lng: 139.44,
   },
-  zoom: 8,
+  zoom: 16,
 };
-const API_KEY: string = process.env.REACT_APP_GOOGLEMAP_API_KEY;
-export const Test = () => {
-  const mapProps = initialMapProps;
+type Spots = {
+  name: string;
+  formatted_address: string;
+  Latitude: string;
+  Longitude: string;
+  placesId: string;
+};
 
+const API_KEY: string = process.env.REACT_APP_GOOGLEMAP_API_KEY;
+
+export const Test = () => {
+  const [text, setText] = useState<String>('');
+  const [map, setMap] = useState(null);
+  const [maps, setMaps] = useState(null);
+  const [spotResults, setSpotResults] = useState<Spots[]>([]);
+  const mapProps = initialMapProps;
   const handleApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
-    const sydney = new maps.LatLng(-33.867, 151.195);
-    const infowindow = new maps.InfoWindow();
-    map = new maps.Map(document.getElementById('map') as HTMLElement, {
-      center: sydney,
-      zoom: 15,
-    });
+    setMap(map);
+    setMaps(maps);
+  };
+  const search = () => {
     const request = {
-      query: '大阪　展望台',
+      query: text,
       fields: ['name', 'geometry'],
       language: 'ja',
     };
-    const service = new maps.places.PlacesService(map);
-
-    service.textSearch(request, (results: any, status: any) => {
-      if (status === maps.places.PlacesServiceStatus.OK && results) {
-        console.log(results);
-        map.setCenter(results[0].geometry!.location!);
-      } else {
-        console.log('あ');
+    if (maps) {
+      if (map) {
+        // @ts-ignore
+        const service = new maps.places.PlacesService(map);
+        service.textSearch(request, (results: any, status: any) => {
+          // @ts-ignore
+          if (status === maps.places.PlacesServiceStatus.OK && results) {
+            console.log(results);
+            const resultList: Spots[] = [];
+            let count: number = 0;
+            results.map((element: any, index: any) => {
+              if (count === index) {
+                const placeInfo: Spots = {
+                  name: element.name,
+                  formatted_address: element.formatted_address,
+                  Latitude: element.geometry.location.lat(),
+                  Longitude: element.geometry.location.lng(),
+                  placesId: element.place_id,
+                };
+                resultList.push(placeInfo);
+                count += 1;
+              }
+            });
+            setSpotResults(resultList);
+          } else {
+            console.log('あ');
+          }
+        });
       }
-    });
+    }
   };
 
   return (
     <>
-      <p>aa</p>
-      <div id="map" style={{ height: '800px', width: '800px' }}>
+      {/* テキストボックス有りver */}
+      <div>
+        <input type="text" onChange={(e) => setText(e.target.value)} />
+        <button type="button" onClick={search}>
+          Search
+        </button>
+      </div>
+
+      <div id="map" style={{ height: '0px', width: '0px' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: API_KEY }}
           center={mapProps.center}
@@ -54,6 +92,19 @@ export const Test = () => {
           yesIWantToUseGoogleMapApiInternals={true}
         />
       </div>
+
+      {spotResults && (
+        <div>
+          {spotResults.map((spots, index) => (
+            <div key={index}>
+              <p>{spots.name}</p>
+              <p>{spots.formatted_address}</p>
+              <p>{spots.Latitude}</p>
+              <p>{spots.Longitude}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </>
   );
 };

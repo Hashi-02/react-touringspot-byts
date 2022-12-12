59eb8d86d9dee2235db88ffea111781407100484
hashi-02 <hassie0209@gmail.com>
2022-12-13 00:08:28 +0900

    Imageコンポーネントにidの受け渡し

--- src/components/pages/DetailPage.tsx
+++ src/components/pages/DetailPage.tsx
@@ -1,7 +1,9 @@
 import { doc, getDoc } from 'firebase/firestore';
+import { getStorage, listAll, ref } from 'firebase/storage';
 import React, { useEffect, useState } from 'react';
 import { db } from '../../firebase';
 import { BaseButton } from '../atoms/button/BaseButton';
+import { ImageLoader } from '../molecules/ImageLoader';
 import { ImageUploader } from '../molecules/ImageUploader';
 
 export const DetailPage = () => {
@@ -11,6 +13,7 @@ export const DetailPage = () => {
     description: string;
     Latitude: string;
     Longitude: string;
+    id: string;
   };
   const [detailInfo, setDetailInfo] = useState<TdetailInfo>();
   useEffect(() => {
@@ -23,6 +26,7 @@ export const DetailPage = () => {
           description: documentSnapshot.data().description,
           Latitude: documentSnapshot.data().Latitude,
           Longitude: documentSnapshot.data().Longitude,
+          id: documentSnapshot.id,
         };
         setDetailInfo(Info);
       }
@@ -33,7 +37,9 @@ export const DetailPage = () => {
       <BaseButton text="マップに戻る" routing="/maps" />
       <div className="container">
         <p>{detailInfo?.placeName}</p>
-        <ImageUploader />
+        <p>{detailInfo?.id}</p>
+        {detailInfo && <ImageUploader id={detailInfo.id} />}
+        {detailInfo && <ImageLoader id={detailInfo.id} />}
       </div>
     </>
   );

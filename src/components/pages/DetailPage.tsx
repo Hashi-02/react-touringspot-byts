8c5800d3e8b7c63f2ebb2155c4995d92d84b2df2
hashi-02 <hassie0209@gmail.com>
2022-12-13 11:19:45 +0900

    useparamsでuidに対応

--- src/components/pages/DetailPage.tsx
+++ src/components/pages/DetailPage.tsx
@@ -1,12 +1,14 @@
 import { doc, getDoc } from 'firebase/firestore';
 import { getStorage, listAll, ref } from 'firebase/storage';
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { db } from '../../firebase';
 import { BaseButton } from '../atoms/button/BaseButton';
 import { ImageLoader } from '../molecules/ImageLoader';
 import { ImageUploader } from '../molecules/ImageUploader';
 
 export const DetailPage = () => {
+  let { uid } = useParams();
   type TdetailInfo = {
     placeName: string;
     file: string;
@@ -17,27 +19,29 @@ export const DetailPage = () => {
   };
   const [detailInfo, setDetailInfo] = useState<TdetailInfo>();
   useEffect(() => {
-    const userDocumentRef = doc(db, 'users', '0Vdlc5IiaAsvXgK3mCZm');
-    getDoc(userDocumentRef).then((documentSnapshot) => {
-      if (documentSnapshot.exists()) {
-        const Info: TdetailInfo = {
-          placeName: documentSnapshot.data().placeName,
-          file: documentSnapshot.data().file,
-          description: documentSnapshot.data().description,
-          Latitude: documentSnapshot.data().Latitude,
-          Longitude: documentSnapshot.data().Longitude,
-          id: documentSnapshot.id,
-        };
-        setDetailInfo(Info);
-      }
-    });
+    const id = uid;
+    if (id) {
+      const userDocumentRef = doc(db, 'users', id);
+      getDoc(userDocumentRef).then((documentSnapshot) => {
+        if (documentSnapshot.exists()) {
+          const Info: TdetailInfo = {
+            placeName: documentSnapshot.data().placeName,
+            file: documentSnapshot.data().file,
+            description: documentSnapshot.data().description,
+            Latitude: documentSnapshot.data().Latitude,
+            Longitude: documentSnapshot.data().Longitude,
+            id: documentSnapshot.id,
+          };
+          setDetailInfo(Info);
+        }
+      });
+    }
   }, []);
   return (
     <>
       <BaseButton text="マップに戻る" routing="/maps" />
       <div className="container">
         <p>{detailInfo?.placeName}</p>
-        <p>{detailInfo?.id}</p>
         {detailInfo && <ImageUploader id={detailInfo.id} />}
         {detailInfo && <ImageLoader id={detailInfo.id} />}
       </div>

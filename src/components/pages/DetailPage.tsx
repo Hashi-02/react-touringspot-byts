3dd7478d741277d9891ebfefc7c38dec9b0201ed
hashi-02 <hassie0209@gmail.com>
2022-12-19 20:47:28 +0900

    DetailPageのスタイル適応

--- src/components/pages/DetailPage.tsx
+++ src/components/pages/DetailPage.tsx
@@ -1,5 +1,4 @@
 import { doc, getDoc } from 'firebase/firestore';
-import { getStorage, listAll, ref } from 'firebase/storage';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { db } from '../../firebase';
@@ -39,11 +38,16 @@ export const DetailPage = () => {
   }, []);
   return (
     <>
-      <BaseButton text="マップに戻る" routing="/maps" />
-      <div className="container">
-        <p>{detailInfo?.placeName}</p>
-        {detailInfo && <ImageUploader id={detailInfo.id} />}
-        {detailInfo && <ImageLoader id={detailInfo.id} />}
+      <div className="mx-10 my-10 ">
+        <BaseButton text="マップに戻る" routing="/maps" />
+        <div className="flex flex-col items-center">
+          <div className="h-3/4">
+            {detailInfo && <ImageLoader id={detailInfo.id} />}
+          </div>
+          <p className="text-7xl font-bold">{detailInfo?.placeName}</p>
+          <p>{detailInfo?.description}</p>
+          {detailInfo && <ImageUploader id={detailInfo.id} />}
+        </div>
       </div>
     </>
   );
